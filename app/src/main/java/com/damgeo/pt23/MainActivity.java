d529a529988516f5b6e5509898cf7b89e0610f84
d <g>
2018-07-31 08:46:24 +0200

    Login

--- app/src/main/java/com/damgeo/pt23/MainActivity.java
+++ app/src/main/java/com/damgeo/pt23/MainActivity.java
@@ -1,13 +1,31 @@
 package com.damgeo.pt23;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button mBasketball;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        mBasketball = (Button) findViewById(R.id.basketball);
+
+        mBasketball.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, BasketballLoginActivity.class);
+                startActivity(intent);
+                finish();
+                return;
+            }
+        });
     }
 }
